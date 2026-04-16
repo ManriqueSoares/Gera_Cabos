1fdf77db09e6eb127ea793a7665dc4dc6438f7bc
Manrique Soares Felix <manriquef@weg.net>
2026-04-16 11:46:34 -0300

    finalizando interface

--- app/layout/pages/home.py
+++ app/layout/pages/home.py
@@ -1,16 +1,24 @@
 import flet as ft
+import time
 from app.layout.widgets.widgets import *
 
 class Home(ft.Container):
     def __init__(self, page: ft.Page):
         super().__init__()
-        self.page = page
-        self.expand = True
-        self.content = ft.Row(
-            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
-            vertical_alignment=ft.CrossAxisAlignment.CENTER,
-            controls=[
-                ft.Container(
+        
+        ## ADD FUNCS AOS BUTTONS
+        BOTAO_CLOSE_SIDBAR.on_click = self.close_sidebar
+        BOTAO_OPEN_SIDBAR.on_click = self.open_sidbar
+        DROPDOWN_SECAO_NOMINAL_DA_LUVA.on_change = self.selection_dropdown_luva
+        DROPDOWN_SECAO_NOMINAL_CABO.on_change = self.selection_dropdown_cabo
+        SLIDER_GRANULARIDADE.on_change = self.slider_granularidade_change
+        SLIDER_LIMITE_OCUPACAO.on_change = self.slider_limite_ocupacao_change
+        ENTRADA_LIMITE_CONFIGURACAO.on_change = self.escrevendo_entrada_limite_ocupacao
+        ENTRADA_TEXT_GRANULARIDADE.on_change = self.escrevendo_entrada_granularidade
+        CHECK_BOX_EXCLUIR_CABO_CIRCULAR.on_change = self.checkbox_excluir_cabo_circular_change
+        BOTAO_ALTERAR_TEMA.on_click = self.alterar_tema
+        ## SIDBAR
+        self.sidbar = ft.Container(
                     width=310,
                     animate=ft.Animation(500, ft.AnimationCurve.DECELERATE),
                     height=True,
@@ -62,28 +70,24 @@ class Home(ft.Container):
                                         DROPDOWN_SECAO_NOMINAL_DA_LUVA,
                                         ENTRADA_DIAMETRO_PERSONALIZADO,
                                         ft.Container(height=10),
-                                        ft.Divider(height=1, color=ft.Colors.GREY_900),
+                                        DIVIDER_SIDEBAR,
                                         ft.Container(height=10),
                                         FIO_RETANGULAR_TITLE,
                                         ENTRADA_TEXT_AXIAL,
                                         ENTRADA_TEXT_RADIAL,
                                         ENTRADA_TEXT_QUANTIDADE,
                                         ft.Container(height=10),    
-                                        ft.Divider(height=1, color=ft.Colors.GREY_900),
+                                        DIVIDER_SIDEBAR,
                                         ft.Container(height=10),
                                         CABO_CIRCULAR_TITLE,
                                         ft.Container(padding=ft.padding.only(left=60), alignment=ft.alignment.center, content=CHECK_BOX_EXCLUIR_CABO_CIRCULAR,),
                                         ft.Container(height=10),    
-                                        ft.Divider(height=1, color=ft.Colors.GREY_900),
+                                        DIVIDER_SIDEBAR,
                                         ft.Container(height=10),
                                         DROPDOWN_SECAO_NOMINAL_CABO,
                                         ENTRADA_SECAO_PERSONALIZADA_CABO,
                                         ft.Container(height=10),    
-                                        ft.Divider(height=1, color=ft.Colors.GREY_900),
-                                        ft.Container(height=10),
-                                        TEXT_DIAMETRO,
-                                        ft.Container(height=10),    
-                                        ft.Divider(height=1, color=ft.Colors.GREY_900),
+                                        DIVIDER_SIDEBAR,
                                         ft.Container(height=10),
                                         ft.Row(spacing=1, alignment=ft.MainAxisAlignment.CENTER, vertical_alignment=ft.CrossAxisAlignment.CENTER, controls=[
                                             ENTRADA_TEXT_GRANULARIDADE,
@@ -91,7 +95,7 @@ class Home(ft.Container):
                                         ]),
                                         SLIDER_GRANULARIDADE,
                                         ft.Container(height=10),    
-                                        ft.Divider(height=1, color=ft.Colors.GREY_900),
+                                        DIVIDER_SIDEBAR,
                                         ft.Container(height=10),
                                         ft.Row(alignment=ft.MainAxisAlignment.CENTER, vertical_alignment=ft.CrossAxisAlignment.CENTER, controls=[
                                             TITULO_CONFIGURACOES,
@@ -100,7 +104,7 @@ class Home(ft.Container):
                                         ENTRADA_LIMITE_CONFIGURACAO,
                                         SLIDER_LIMITE_OCUPACAO,
                                         ft.Container(height=10),    
-                                        ft.Divider(height=1, color=ft.Colors.GREY_900),
+                                        DIVIDER_SIDEBAR,
                                         ft.Container(height=10),
                                     ]
                                 )
@@ -134,7 +138,197 @@ class Home(ft.Container):
                             )
                         ]
                     )
-                ),
+                )
+
+        """  --------------------------------------------------------- CONFIG FRAME ---------------------------------------------------------  """
+        self.page = page
+        self.expand = True
+        self.content = self.build()
+        """  --------------------------------------------------------------------------------------------------------------------------------  """
+
+    def alterar_tema(self, e):
+        if self.page.theme_mode == "light":
+            self.page.theme_mode = "dark"
+            self.sidbar.bgcolor = ft.Colors.with_opacity(0.8, ft.Colors.BLUE_GREY_900)
+            DIVIDER_SIDEBAR.color = ft.Colors.GREY_900
+        else:
+            self.page.theme_mode = "light"
+            self.sidbar.bgcolor = ft.Colors.with_opacity(0.8, ft.Colors.BLUE_GREY_100)
+            DIVIDER_SIDEBAR.color = ft.Colors.GREY_200
+        
+        DIVIDER_SIDEBAR.update()
+        self.page.update()
+
+    def open_sidbar(self, e):
+        self.sidbar.width = 310
+        BOTAO_OPEN_SIDBAR.visible = False
+        BOTAO_OPEN_SIDBAR.update()
+        self.sidbar.update()
+
+    def close_sidebar(self, e):
+        self.sidbar.width = 0.0
+        BOTAO_OPEN_SIDBAR.visible = True
+        BOTAO_OPEN_SIDBAR.update()
+        self.sidbar.update()
+
+    def slider_granularidade_change(self, e):
+        ENTRADA_TEXT_GRANULARIDADE.value = f"{SLIDER_GRANULARIDADE.value:.2f}"
+        SLIDER_GRANULARIDADE.label = f"{SLIDER_GRANULARIDADE.value:.2f}"
+        ENTRADA_TEXT_GRANULARIDADE.update()
+        SLIDER_GRANULARIDADE.update()
+
+    def slider_limite_ocupacao_change(self, e):
+        ENTRADA_LIMITE_CONFIGURACAO.value = f"{SLIDER_LIMITE_OCUPACAO.value:.0f}"
+        SLIDER_LIMITE_OCUPACAO.label = f"{SLIDER_LIMITE_OCUPACAO.value:.0f}"
+        ENTRADA_LIMITE_CONFIGURACAO.update()
+        SLIDER_LIMITE_OCUPACAO.update()
+
+    def escrevendo_entrada_limite_ocupacao(self, e):
+        if ENTRADA_LIMITE_CONFIGURACAO.error_text != None:
+            ENTRADA_LIMITE_CONFIGURACAO.error_text = None
+            ENTRADA_LIMITE_CONFIGURACAO.update()
+        try:
+            valor = float(ENTRADA_LIMITE_CONFIGURACAO.value)
+            if 0 <= valor <= 100:
+                SLIDER_LIMITE_OCUPACAO.value = valor
+                SLIDER_LIMITE_OCUPACAO.label = f"{valor:.0f}"
+                SLIDER_LIMITE_OCUPACAO.update()
+            else:
+                ENTRADA_LIMITE_CONFIGURACAO.error_text = "Valor deve ser entre 0 e 100."
+                ENTRADA_LIMITE_CONFIGURACAO.update()
+        except ValueError:
+            pass
+            
+    def escrevendo_entrada_granularidade(self, e):
+
+        if ENTRADA_TEXT_GRANULARIDADE.error_text != None:
+            ENTRADA_TEXT_GRANULARIDADE.error_text = None
+            ENTRADA_TEXT_GRANULARIDADE.update()
+
+        try:
+            valor = float(ENTRADA_TEXT_GRANULARIDADE.value)
+            if 0.4 <= valor <= 2.0:
+                SLIDER_GRANULARIDADE.value = valor
+                SLIDER_GRANULARIDADE.label = f"{valor:.2f}"
+                SLIDER_GRANULARIDADE.update()
+            else:
+                ENTRADA_TEXT_GRANULARIDADE.error_text = "Valor deve ser entre 0.40 e 2.00."
+                ENTRADA_TEXT_GRANULARIDADE.update()
+
+        except ValueError:
+            pass
+
+    def selection_dropdown_luva(self, e):
+        match DROPDOWN_SECAO_NOMINAL_DA_LUVA.value:
+            case "25 mm²":
+                ENTRADA_DIAMETRO_PERSONALIZADO.value = 8.0
+                ENTRADA_DIAMETRO_PERSONALIZADO.disabled = True
+            case "35 mm²":
+                ENTRADA_DIAMETRO_PERSONALIZADO.value = 9.0
+                ENTRADA_DIAMETRO_PERSONALIZADO.disabled = True
+            case "50 mm²":
+                ENTRADA_DIAMETRO_PERSONALIZADO.value = 11.0
+                ENTRADA_DIAMETRO_PERSONALIZADO.disabled = True
+            case "70 mm²":
+                ENTRADA_DIAMETRO_PERSONALIZADO.value = 13.0
+                ENTRADA_DIAMETRO_PERSONALIZADO.disabled = True
+            case "95 mm²":
+                ENTRADA_DIAMETRO_PERSONALIZADO.value = 15.0
+                ENTRADA_DIAMETRO_PERSONALIZADO.disabled = True
+            case "120 mm²":
+                ENTRADA_DIAMETRO_PERSONALIZADO.value = 17.0
+                ENTRADA_DIAMETRO_PERSONALIZADO.disabled = True
+            case "150 mm²":
+                ENTRADA_DIAMETRO_PERSONALIZADO.value = 19.0
+                ENTRADA_DIAMETRO_PERSONALIZADO.disabled = True
+            case "185 mm²":
+                ENTRADA_DIAMETRO_PERSONALIZADO.value = 21.0
+                ENTRADA_DIAMETRO_PERSONALIZADO.disabled = True
+            case "240 mm²":
+                ENTRADA_DIAMETRO_PERSONALIZADO.value = 24.0
+                ENTRADA_DIAMETRO_PERSONALIZADO.disabled = True
+            case "300 mm²":
+                ENTRADA_DIAMETRO_PERSONALIZADO.value = 24.5
+                ENTRADA_DIAMETRO_PERSONALIZADO.disabled = True
+            case "400 mm²":
+                ENTRADA_DIAMETRO_PERSONALIZADO.value = 30.0
+                ENTRADA_DIAMETRO_PERSONALIZADO.disabled = True
+            case "500 mm²":
+                ENTRADA_DIAMETRO_PERSONALIZADO.value = 33.0
+                ENTRADA_DIAMETRO_PERSONALIZADO.disabled = True
+            case "630 mm²":
+                ENTRADA_DIAMETRO_PERSONALIZADO.value = 39.0
+                ENTRADA_DIAMETRO_PERSONALIZADO.disabled = True
+            case "Personalizado":
+                ENTRADA_DIAMETRO_PERSONALIZADO.value = 0.0
+                ENTRADA_DIAMETRO_PERSONALIZADO.disabled = False
+                pass
+        ENTRADA_DIAMETRO_PERSONALIZADO.visible=True
+        ENTRADA_DIAMETRO_PERSONALIZADO.update()
+    
+    def selection_dropdown_cabo(self, e):
+        match DROPDOWN_SECAO_NOMINAL_CABO.value:
+            case "50 mm²":
+                ENTRADA_SECAO_PERSONALIZADA_CABO.value = 9.15
+                ENTRADA_SECAO_PERSONALIZADA_CABO.disabled = True
+            case "70 mm²":
+                ENTRADA_SECAO_PERSONALIZADA_CABO.value = 10.83
+                ENTRADA_SECAO_PERSONALIZADA_CABO.disabled = True
+            case "120 mm²":
+                ENTRADA_SECAO_PERSONALIZADA_CABO.value = 14.77
+                ENTRADA_SECAO_PERSONALIZADA_CABO.disabled = True
+            case "185 mm²":
+                ENTRADA_SECAO_PERSONALIZADA_CABO.value = 18.09
+                ENTRADA_SECAO_PERSONALIZADA_CABO.disabled = True
+            case "240 mm²":
+                ENTRADA_SECAO_PERSONALIZADA_CABO.value = 23.3
+                ENTRADA_SECAO_PERSONALIZADA_CABO.disabled = True
+            case "300 mm²":
+                ENTRADA_SECAO_PERSONALIZADA_CABO.value = 23.5
+                ENTRADA_SECAO_PERSONALIZADA_CABO.disabled = True
+            case "400 mm²":
+                ENTRADA_SECAO_PERSONALIZADA_CABO.value = 29.1
+                ENTRADA_SECAO_PERSONALIZADA_CABO.disabled = True
+            case "500 mm²":
+                ENTRADA_SECAO_PERSONALIZADA_CABO.value = 33.5
+                ENTRADA_SECAO_PERSONALIZADA_CABO.disabled = True
+            case "Personalizado":
+                ENTRADA_SECAO_PERSONALIZADA_CABO.value = 0.0
+                ENTRADA_SECAO_PERSONALIZADA_CABO.disabled = False
+                pass
+        ENTRADA_SECAO_PERSONALIZADA_CABO.visible=True
+        ENTRADA_SECAO_PERSONALIZADA_CABO.update()
+
+    def checkbox_excluir_cabo_circular_change(self, e):
+        if CHECK_BOX_EXCLUIR_CABO_CIRCULAR.value == True:
+            TITULO_JANELA_PRINCIPAL.value = "Simulação: Fio Retangular"
+            DROPDOWN_SECAO_NOMINAL_CABO.visible = False
+            ENTRADA_SECAO_PERSONALIZADA_CABO.visible = False
+            ENTRADA_TEXT_GRANULARIDADE.visible = False
+            ICONE_PERGUNTA_GRANULARIDADE.visible = False
+            SLIDER_GRANULARIDADE.visible = False
+        else:
+            TITULO_JANELA_PRINCIPAL.value = "Simulação: Fio Retangular + Cabo Circular"
+            DROPDOWN_SECAO_NOMINAL_CABO.visible = True
+            ENTRADA_SECAO_PERSONALIZADA_CABO.visible = True
+            ENTRADA_TEXT_GRANULARIDADE.visible = True
+            ICONE_PERGUNTA_GRANULARIDADE.visible = True
+            SLIDER_GRANULARIDADE.visible = True
+
+        TITULO_JANELA_PRINCIPAL.update()
+        DROPDOWN_SECAO_NOMINAL_CABO.update()
+        ENTRADA_SECAO_PERSONALIZADA_CABO.update()
+        ENTRADA_TEXT_GRANULARIDADE.update()
+        ICONE_PERGUNTA_GRANULARIDADE.update()
+        SLIDER_GRANULARIDADE.update()
+
+
+    def build(self):
+        return ft.Row(
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                self.sidbar,
                 ft.Container(
                     expand=True,
                     content=ft.Column(
@@ -155,7 +349,7 @@ class Home(ft.Container):
                                     horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                                     controls=[
                                         ft.Row(alignment=ft.MainAxisAlignment.START, vertical_alignment=ft.CrossAxisAlignment.CENTER, controls=[
-                                            TITULO_JANELA_PRINCIPAL
+                                            BOTAO_OPEN_SIDBAR, ft.Container(width=20), TITULO_JANELA_PRINCIPAL
                                         ]),
                                         ft.Row(alignment=ft.MainAxisAlignment.START, vertical_alignment=ft.CrossAxisAlignment.CENTER, controls=[
                                             DESCRICAO_APP
@@ -361,8 +555,6 @@ class Home(ft.Container):
                                 padding=ft.padding.only(right=15, left=15),
                                 content=CONTAINER_VALIDACAO_SIMULACAO
                             ),
-                            
-                            #ft.Divider(height=1, color=ft.Colors.GREY_900),
                             ## LOGS
                             ft.Container(
                                 width=True,
--- app/layout/widgets/widgets.py
+++ app/layout/widgets/widgets.py
@@ -5,6 +5,7 @@ import flet as ft
 BOTAO_CLOSE_SIDBAR = ft.IconButton(icon=ft.CupertinoIcons.LEFT_CHEVRON)
 LOGO_WEG = ft.Image(src="assets/icon.png", width=80) ## Verificar verdadeira posição
 
+DIVIDER_SIDEBAR = ft.Divider(height=1, color="transparent")
 PARAMETROS_DA_LUVA_TITLE = ft.Text("Parâmetros da Luva", size=15, weight=ft.FontWeight.W_500)
 DROPDOWN_SECAO_NOMINAL_DA_LUVA = ft.Dropdown(label="Seção Nominal da Luva", width=300, border_radius=10, label_style=ft.TextStyle(12), text_style=ft.TextStyle(12), scale=0.88, editable=True, input_filter=True, enable_filter=True, options=[
     ft.dropdown.Option("Automático"),
@@ -24,7 +25,7 @@ DROPDOWN_SECAO_NOMINAL_DA_LUVA = ft.Dropdown(label="Seção Nominal da Luva", wi
     ft.dropdown.Option("Personalizado"),
 ])
 
-ENTRADA_DIAMETRO_PERSONALIZADO = ft.TextField(label="Personalizado (mm)", width=300, border_radius=10, scale=0.88, label_style=ft.TextStyle(size=12), text_style=ft.TextStyle(size=12), visible=False)
+ENTRADA_DIAMETRO_PERSONALIZADO = ft.TextField(label="Diâmetro da Luva (mm)", width=300, border_radius=10, scale=0.88, label_style=ft.TextStyle(size=12), text_style=ft.TextStyle(size=12), visible=False)
 
 FIO_RETANGULAR_TITLE = ft.Text("Fio Retangular", size=13, weight=ft.FontWeight.W_500)
 ENTRADA_TEXT_AXIAL = ft.TextField(label="Axial (mm)", width=300, border_radius=10, scale=0.88, label_style=ft.TextStyle(size=12), text_style=ft.TextStyle(size=12))
@@ -46,18 +47,18 @@ DROPDOWN_SECAO_NOMINAL_CABO = ft.Dropdown(label="Seção Nominal do Cabo", width
     ft.dropdown.Option("Personalizado"),
 ])
 
-ENTRADA_SECAO_PERSONALIZADA_CABO = ft.TextField(label="Personalizada (mm²)", width=300, border_radius=10, scale=0.88, label_style=ft.TextStyle(size=12), text_style=ft.TextStyle(size=12), visible=False)
+ENTRADA_SECAO_PERSONALIZADA_CABO = ft.TextField(label="Diâmetro equivalente total (mm)", width=300, border_radius=10, scale=0.88, label_style=ft.TextStyle(size=12), text_style=ft.TextStyle(size=12), visible=False)
 
 TEXT_DIAMETRO = ft.Text("Diâmetro 9.15 (mm)", size=13, weight=ft.FontWeight.W_500)
 
-ENTRADA_TEXT_GRANULARIDADE = ft.TextField(label="Diâmetro do Micro-fio (Granularidade)", width=250, border_radius=10, scale=0.88, label_style=ft.TextStyle(size=12), text_style=ft.TextStyle(size=12))
-ICONE_PERGUNTA_GRANULARIDADE = ft.IconButton(icon=ft.Icons.HELP, icon_size=17, highlight_color="transparent", tooltip="O cabo circular é multifilamentoso (composto por vários fios finos). O diâmetro selecionado será o diâmetro da 'partição' criada na simulação. Quanto menor, mais preciso o preenchimento, porém mais lenta a simulação.")
-SLIDER_GRANULARIDADE = ft.Slider(min=0.4, max=2.0, value=1.0, divisions=49, width=250)
+ENTRADA_TEXT_GRANULARIDADE = ft.TextField(label="Diâmetro do Micro-fio (Granularidade)", value="0.80", width=250, border_radius=10, scale=0.88, label_style=ft.TextStyle(size=12), text_style=ft.TextStyle(size=12))
+ICONE_PERGUNTA_GRANULARIDADE = ft.Icon(ft.Icons.HELP_OUTLINE, size=17, color="#888888", tooltip="O cabo circular é multifilamentoso (composto por vários fios finos). O diâmetro selecionado será o diâmetro da 'partição' criada na simulação. Quanto menor, mais preciso o preenchimento, porém mais lenta a simulação.")
+SLIDER_GRANULARIDADE = ft.Slider(min=0.4, max=2.0, value=0.80, divisions=160, width=250, scale=0.7)
 
 TITULO_CONFIGURACOES = ft.Text("Configurações", size=13, weight=ft.FontWeight.W_500)
-ENTRADA_LIMITE_CONFIGURACAO = ft.TextField(label="Limite de Ocupação (%)", width=300, border_radius=10, scale=0.88, label_style=ft.TextStyle(size=12), text_style=ft.TextStyle(size=12))
-ICONE_PERGUNTA_CONFIGURACOES = ft.IconButton(icon=ft.Icons.HELP, icon_size=17, highlight_color="transparent", tooltip="Taxa de ocupação é a porcentagem da área interna da luva que é preenchida pelos condutores. O padrão é 85%, conforme WPR-33156 ES (WEG México).")
-SLIDER_LIMITE_OCUPACAO = ft.Slider(min=0.0, max=100.0, value=85.0, divisions=100, width=250)
+ENTRADA_LIMITE_CONFIGURACAO = ft.TextField(label="Limite de Ocupação (%)", value="85", width=300, border_radius=10, scale=0.88, label_style=ft.TextStyle(size=12), text_style=ft.TextStyle(size=12))
+ICONE_PERGUNTA_CONFIGURACOES = ft.Icon(ft.Icons.HELP_OUTLINE, size=17, color="#888888", tooltip="Taxa de ocupação é a porcentagem da área interna da luva que é preenchida pelos condutores. O padrão é 85%, conforme WPR-33156 ES (WEG México).")
+SLIDER_LIMITE_OCUPACAO = ft.Slider(min=0.0, max=100.0, value=85.0, divisions=100, width=250, scale=0.7)
 
 ELEVATE_BUTTON_EXECUTAR_SIMULACAO = ft.ElevatedButton("Executar Simulação", width=150, height=30, bgcolor=ft.Colors.with_opacity(0.8,ft.Colors.BLUE_800), color="white")
 
@@ -65,6 +66,7 @@ BOTAO_ALTERAR_TEMA = ft.IconButton(icon=ft.Icons.BRIGHTNESS_4, tooltip="Alterar 
 
 
 """ ----------------------------------------------------- Janela Principal ----------------------------------------------------- """
+BOTAO_OPEN_SIDBAR = ft.IconButton(icon=ft.CupertinoIcons.RIGHT_CHEVRON, visible=False, animate_opacity=1000)
 TITULO_JANELA_PRINCIPAL = ft.Text("Simulação: Fio Retangular + Cabo Circular", size=27, weight=ft.FontWeight.W_600) ## Precisa ser alterado em algumas situaçãoes específicas
 DESCRICAO_APP = ft.Text("Este aplicativo simula a montagem prática de luvas, verificando fisicamente se o condutor cabe na luva. Além disso, realiza a simulação \nutilizando cabos retangulares e circulares para validar o encaixe em luvas curtas.", size=16)
 
@@ -73,14 +75,14 @@ PROGRESS_BAR_SIMULACAO = ft.ProgressBar(width=True, height=8, color=ft.Colors.BL
 TEXT_DADOS_DAS_SECOES = ft.Text("Dados das Seções", size=20, weight=ft.FontWeight.W_500)
 
 TITULO_FIO_RETANGULAR = ft.Text("Fio Retangular", size=13, weight=ft.FontWeight.W_400)
-ICONE_PERGUNTA_FIO_RETANGULAR = ft.Icon(ft.Icons.HELP, color="#888888", size=18, tooltip="Unitário: 7.0x1.5 mm | Qtd: 10")
+ICONE_PERGUNTA_FIO_RETANGULAR = ft.Icon(ft.Icons.HELP_OUTLINE, color="#888888", size=18, tooltip="Unitário: 7.0x1.5 mm | Qtd: 10")
 VALOR_FIO_RETANGULAR = ft.Text("105.00 mm²", size=20, weight=ft.FontWeight.W_500)
 
 TITULO_CABO_CIRCULAR = ft.Text("Cabo Circular", size=13, weight=ft.FontWeight.W_400)
 VALOR_CABO_CIRCULAR = ft.Text("95.00 mm²", size=20, weight=ft.FontWeight.W_500)
 
 TITULO_LUVA = ft.Text("Luva", size=13, weight=ft.FontWeight.W_400)
-ICONE_PERGUNTA_LUVA = ft.Icon(ft.Icons.HELP, color="#888888", size=18, tooltip="Diâmetro: 17.00 mm")
+ICONE_PERGUNTA_LUVA = ft.Icon(ft.Icons.HELP_OUTLINE, color="#888888", size=18, tooltip="Diâmetro: 17.00 mm")
 VALOR_LUVA = ft.Text("226.98 mm²", size=20, weight=ft.FontWeight.W_500)
 
 TEXTO_INDICADORES = ft.Text("Indicadores", size=20, weight=ft.FontWeight.W_500)
@@ -90,7 +92,7 @@ VALOR_INDICADOR_FIOS = ft.Text("10/10", size=20, weight=ft.FontWeight.W_500)
 
 TITULO_OCUPACAO = ft.Text("Ocupação", size=13, weight=ft.FontWeight.W_400)
 VALOR_INDICADOR_OCUPACAO = ft.Text("46.40%", size=20, weight=ft.FontWeight.W_500)
-ICONE_DESVIO_OCUPACAO = ft.Icon(ft.CupertinoIcons.ARROW_RIGHT, size=10)
+ICONE_DESVIO_OCUPACAO = ft.Icon(ft.CupertinoIcons.ARROW_RIGHT, size=10, color=ft.Colors.GREEN_200)
 VALOR_DESVIO_OCUPACAO = ft.Text("0.00%", size=10, weight=ft.FontWeight.W_500, color=ft.Colors.GREEN_200)
 CONTAINER_DESVIO_OCUPACAO = ft.Container(
     width=70,
@@ -136,4 +138,4 @@ LOG_CONCLUSAO_SIMULACAO = ft.Text("Simulação concluída! (Total: 1.14s)", size
 BOTAO_BAIXAR_RELATORIO = ft.ElevatedButton(text="Baixar Relatório", width=150, bgcolor=ft.Colors.RED, height=26, icon=ft.Icons.FILE_OPEN)
 
 TITULO_DESENVOLVEDOR = ft.Text("Desenvolvedor", size=14, weight=ft.FontWeight.W_400)
-VALOR_DESENVOLVEDOR = ft.Text("DELLAZARING - Parte Ativa Gravataí", size=14, weight=ft.FontWeight.W_500)
+VALOR_DESENVOLVEDOR = ft.Text("DELLAZARIG - Parte Ativa Gravataí", size=14, weight=ft.FontWeight.W_500)
